669658662fdee4e80ebe4f31662c2cbe83cbd72d
lavkushtyagi <lavkushtyagi22@gmail.com>
2023-10-06 16:36:49 +0530

    changes

--- zipSign/Controllers/LoginController.cs
+++ zipSign/Controllers/LoginController.cs
@@ -1001,7 +1001,6 @@ namespace zipSign.Controllers
 
         public string SendLinkviaEmail(string Email, string UserCode)
         {
-
             string EncUserCode = AESEncryption.AESEncryptionClass.EncryptAES(Convert.ToString(UserCode));
             string LinkText = GenerateResetLink(EncUserCode);
 
